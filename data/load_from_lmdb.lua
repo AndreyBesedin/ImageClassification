739f8de187a560d00d5130f0ddac82976f359be6
AndreyBesedin <andreibse89@gmail.com>
2017-12-20 09:16:43 +0100

    created a tool to extract features from LSUN using pretrained resnet200

--- data/load_from_lmdb.lua
+++ data/load_from_lmdb.lua
@@ -1,2 +1,29 @@
-local DataLoader = paths.dofile('./data.lua')
-local data = DataLoader.new(opt.nThreads, 'lsun', opt)
+require 'cunn'
+require 'cudnn'
+opt = {                                                                                                                                                  
+   dataset = 'lsun',       -- imagenet / lsun / folder
+   batchSize = 64,
+   loadSize = 225,
+   fineSize = 224,
+   nThreads = 6,           -- #  of data loading threads to use
+   ntrain = math.huge,     -- #  of examples per epoch. math.huge for full dataset
+   gpu = 1,                -- gpu = 0 is CPU mode. gpu=X is GPU mode on GPU X
+}
+opt.data_classes_full = {'bedroom', 'bridge', 'church_outdoor', 'classroom', 'conference_room',
+                         'dining_room', 'kitchen', 'living_room', 'restaurant', 'tower'}
+local feature_extractor = torch.load('/home/besedin/workspace/Models/ResNet_torch/resnet-200.t7')
+feature_extractor:remove(14); feature_extractor:remove(13)
+for idx_class = 1, #opt.data_classes_full do
+  opt.data_classes = {opt.data_classes_full[idx_class]}
+  local DataLoader = paths.dofile('./data.lua')
+  local data = DataLoader.new(opt.nThreads, 'lsun', opt)
+  local res = torch.CudaTensor(data:size(1), 2048)
+  for idx_data = 1, data:size(1), opt.batchSize do
+    xlua.progress(idx_data, data:size())
+    batch = data:getBatch()
+    batch = batch:cuda()
+    res[{{idx_data, idx_data + opt.batchSize - 1},{}}] = feature_extractor:forward(batch)
+  end
+  torch.save(opt.data_classes[1] .. '_50k_features.t7', res)
+end
+
